404e5c794b5524de809e60ae8e74956ba507507a
Bartan02 <72754028+Bartan02@users.noreply.github.com>
2020-10-25 13:13:38 +0100

    Update world.js

--- world.js
+++ world.js
@@ -14,20 +14,20 @@ function draw() {
     }
   //2 w gore punkty
 	if (frameCount > 450){
-		if((frameCount - 84) % 150 == 0)
-			points++;
-		console.log(points);
+	if((frameCount - 191) % 150 == 0)
+		points++;
+		document.getElementById("wynik").innerText = points;
 	}
 	//1 punkt
 	if (frameCount == 380){
 		points++;
-		console.log(points);
 	}
     for (var pipe of pipes) {
         pipe.show();
         pipe.update();
         if (bird.touches(pipe)) {
             noLoop()
+		console.log("GAME OVER");
         }
     }
     bird.show();
